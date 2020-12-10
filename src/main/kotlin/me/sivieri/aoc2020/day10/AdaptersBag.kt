fe620dcd08aa7cdba5c00d96df1714cc55acf83b
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2020-12-10 18:18:22 +0100

    Day 10 part 1

--- src/main/kotlin/me/sivieri/aoc2020/day10/AdaptersBag.kt
+++ src/main/kotlin/me/sivieri/aoc2020/day10/AdaptersBag.kt
@@ -5,7 +5,15 @@ class AdaptersBag(
 ) {
 
     fun findDifferenceProduct(): Int {
-        TODO()
+        val sortedAdapters = adapters
+            .plus(0)
+            .plus(adapters.maxOrNull()!! + 3)
+            .sorted()
+        val diffs = sortedAdapters
+            .subList(0, sortedAdapters.size - 1)
+            .zip(sortedAdapters.subList(1, sortedAdapters.size))
+            .map { it.second - it.first }
+        return diffs.filter { it == 1 }.size * diffs.filter { it == 3 }.size
     }
 
 }
--- src/main/kotlin/me/sivieri/aoc2020/day10/Main1.kt
+++ src/main/kotlin/me/sivieri/aoc2020/day10/Main1.kt
@@ -1,10 +1,17 @@
 package me.sivieri.aoc2020.day10
 
+import me.sivieri.aoc2020.Utils
+
 object Main1 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-
+        val data = Utils
+            .readInput(10)
+            .map { it.toInt() }
+        val bag = AdaptersBag(data)
+        val res = bag.findDifferenceProduct()
+        println(res)
     }
 
 }
