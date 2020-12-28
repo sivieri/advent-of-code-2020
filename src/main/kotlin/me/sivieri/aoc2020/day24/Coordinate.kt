a2da7bff0f734529f6eeb905ebdacf8ba258a400
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2020-12-28 12:22:01 +0100

    Day 24 part 1

--- src/main/kotlin/me/sivieri/aoc2020/day24/Coordinate.kt
+++ src/main/kotlin/me/sivieri/aoc2020/day24/Coordinate.kt
@@ -1,20 +1,29 @@
 package me.sivieri.aoc2020.day24
 
-import me.sivieri.aoc2020.round
-import kotlin.math.sqrt
-
 data class Coordinate(
-    val x: Double,
-    val y: Double
+    val x: Int,
+    val y: Int
 ) {
 
     fun move(direction: Direction): Coordinate = when (direction) {
-        Direction.e -> this.copy(x = x + 2)
-        Direction.se -> this.copy(x = x + 1, y = (y - sqrt(2.0)).round(2))
-        Direction.sw -> this.copy(x = x - 1, y = (y - sqrt(2.0)).round(2))
-        Direction.w -> this.copy(x = x - 2)
-        Direction.nw -> this.copy(x = x - 1, y = (y + sqrt(2.0)).round(2))
-        Direction.ne -> this.copy(x = x + 1, y = (y + sqrt(2.0)).round(2))
+        Direction.e -> this.copy(x = x + 1)
+        Direction.w -> this.copy(x = x - 1)
+        Direction.se -> {
+            if (y % 2 == 0) this.copy(y = y + 1)
+            else this.copy(x = x + 1, y = y + 1)
+        }
+        Direction.sw -> {
+            if (y % 2 == 0) this.copy(x = x - 1, y = y + 1)
+            else this.copy(y = y + 1)
+        }
+        Direction.nw -> {
+            if (y % 2 == 0) this.copy(x = x - 1, y = y - 1)
+            else this.copy(y = y - 1)
+        }
+        Direction.ne -> {
+            if (y % 2 == 0) this.copy(y = y - 1)
+            else this.copy(x = x + 1, y = y - 1)
+        }
     }
 
 }
