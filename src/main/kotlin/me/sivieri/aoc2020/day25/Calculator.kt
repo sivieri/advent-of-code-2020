0fc28f69337e25bb5010703554cd5fa9860cd9f9
Alessandro Sivieri <alessandro.sivieri@gmail.com>
2020-12-31 17:30:21 +0100

    Day 25 part 1

--- src/main/kotlin/me/sivieri/aoc2020/day25/Calculator.kt
+++ src/main/kotlin/me/sivieri/aoc2020/day25/Calculator.kt
@@ -2,23 +2,33 @@ package me.sivieri.aoc2020.day25
 
 class Calculator {
 
-    fun transform(subject: Long, loopSize: Int): Long {
-        var value: Long = 1
-        for (i in 1..loopSize) {
-            value = (value * subject) % multiplier
+    fun transform(subject: Long, loopSize: Int): Long =
+        transform(1L, subject, 1, loopSize)
+
+    private fun transform(
+        startingValue: Long,
+        subject: Long,
+        startingLoop: Int,
+        loopSize: Int
+    ): Long {
+        var value = startingValue
+        for (i in startingLoop..loopSize) {
+            value = transformValue(value, subject)
         }
         return value
     }
 
+    private fun transformValue(value: Long, subject: Long) = (value * subject) % multiplier
+
     fun searchLoopSize(subject: Long, expected: Long): Int {
         var loopSize = 1
-        var res = transform(subject, loopSize)
+        var res = 1L
         while (res != expected) {
-            loopSize++
             if (loopSize % 1000 == 0) println("Loop size $loopSize")
-            res = transform(subject, loopSize)
+            res = transformValue(res, subject)
+            loopSize++
         }
-        return loopSize
+        return loopSize - 1
     }
 
     fun calculateEncryptionKey(loopSize: Int, publicKey: Long): Long =
